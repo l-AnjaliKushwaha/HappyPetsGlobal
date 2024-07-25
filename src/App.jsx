40d84116b427a08111f17233b79667950e4b2a9c
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-07-25 16:30:22 +0530

    "Added fontawesome-free dependency, updated App and Navbar components, and removed vite.config.js file"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { React } from 'react'
-import NavBar from './Components/Navbar/Navbar.jsx'
+import React from "react";
+import NavBar from "./Components/Navbar/Navbar.jsx";
+import "./index.css";
+import Home from "./Components/Home/Home.jsx";
 
 function App() {
-  
   return (
     <>
-     <NavBar/>
+      <NavBar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
