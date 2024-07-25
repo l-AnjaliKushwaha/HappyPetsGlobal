4b231a7285346465a15b85557a560aa2f507062c
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-07-25 18:51:22 +0530

    second section page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./Components/Navbar/Navbar.jsx";
 import "./index.css";
 import Home from "./Components/Home/Home.jsx";
+import WeDo from "./Components/WeDo/WeDo.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <WeDo/>
     </>
   );
 }
