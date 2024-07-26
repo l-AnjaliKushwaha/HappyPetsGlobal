f7694115540c803a346c1d63b18666a2fdb74ce7
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-07-26 19:12:30 +0530

    "Updated Service component CSS and JSX files with new styles and HTML structure for service sections."

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Service.css'
+import React from "react";
+import "./Service.css";
 
 function Service() {
   return (
@@ -24,8 +24,36 @@ function Service() {
             </div>
           </div>
           <div className="s-right">
-            <div className="div-1 right-box"></div>
-            <div className="div-2 right-box"></div>
+            <div className="div-1 right-box">
+              <div className="r-div1-overlay">
+                <div className="div1-left">
+                  <p>A home away from home for your furry friends</p>
+                  <button className="btn"> Pet's Shelter</button>
+                </div>
+                <div className="div1-right">
+                  <img
+                    width={240}
+                    height={152}
+                    src="https://s3-alpha-sig.figma.com/img/6f18/8e21/b59c40570cb36c7e14ff04d96a97265c?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jJvk6IQqrTTI2Va~9lf3dUxOtmEzlCmRgGuFMbM4Uh7ClcEk05gArE5rCj-A9QuX6CcrEVPwtoYwB-fb4E2vDnSnu3H-p5PK7vwiMo~O0PReuELYDn0Bk~hc1WFtkf3mE4PgDsE9XoZHpnWr1UrZm1TR3p26K2nBanTuHlRbAVJYdgx--9cHwh9~CLdf9bXt-WOyXOKBjUMRIjvyFCUDIwhWWyJ2F6az5ljJ3A3P73C4Mne8SvzzcArJUw7zekBPrJsXrDaK2~8LDwt3RXgE9xoBbozeh7UhoVUKZNxU2HAjPt0fRROQuHXr4lLSQpv1QJaaQzKLVrySnDhZcOfSyw__"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="div-2 right-box">
+              <div className="r-div2-overlay">
+                <div className="div2-left">
+                  <p>Transform your pet with our expert training programs</p>
+                  <button className="btn">Pet's Training</button>
+                </div>
+                <div className="div2-right">
+                  <img width={280} height={172}
+                    src="https://s3-alpha-sig.figma.com/img/ea25/7a3f/c23588fc159cbddb135c82959392599f?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H5QZrGa4AgcgCgTv2kP53PwJbLHs7sxEepreiobh5izn1LFkURmoe7y~liCt8zcO2LzCWi~J620FtxFbXEdqONzl7VwxsNpxHkRC9pXBviGbadA-VSa5rAVFaeK3juW5RBUXM9vcl67lPdXPzTpmyXdcsOaHEATF~biq6cfOJw7KioYhPQHUC1ykZBJv-pAsjuzCMGYd0kZvmoA3PbveLQu5ak0LktBe2NrmXnjn3XeQWS9~ImiDlPGO~c3P68hD4p0FTayeyYQt3hmbdQ72N~D4unbKJyPF1lh3HW7sBp~l0EqoUSI9iIWtULCNFpm1yPIbnLeftTB5ZPGeJL6~Bw__"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -33,4 +61,4 @@ function Service() {
   );
 }
 
-export default Service
+export default Service;
