cb5d05e0b11d5f5d01327a587de74e362a0a4fbf
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-10-19 22:16:45 +0530

    hero page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import backgroundimg from "../assets/Images/BackgroundImg.png"
-import Page1
+import dog from "../assets/Images/Page1Dog.png"
+import girl from "../assets/Images/Page1Girl.png"
 
 function Home() {
   return (
     <>
       <div className="home">
         <div
-          className="hero-page1 h-[70vh] bg-[#f5eacd9a] "
+          className="hero-page1 h-[70vh] bg-[#f5eacd9a] flex justify-between"
           style={{ backgroundImage: `url(${backgroundimg})` }}
         >
-          <div className='dog'>
-            <img src={} alt="" />
+          <div className="dog">
+            <img src={dog} alt="" />
           </div>
-          <div className='center-content'></div>
-          <div className='girl'>
-
+          <div className="center-content">
+            <p>Give Your Pets</p>
+            <h1>An Everyday Wagging Tail Experience </h1>
+          </div>
+          <div
+            className="girl w-[30vw] h-auto bg-[#D0A8A8] rounded-xl"
+          >
+            <img src={girl} alt="" />
           </div>
         </div>
       </div>
@@ -24,3 +30,5 @@ function Home() {
 }
 
 export default Home
+
+
